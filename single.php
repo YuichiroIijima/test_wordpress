7cf2a263124f91df61ff2cb4c038e523dd0c0124
Yuichiro Iijima <jimaichiro@gmail.com>
2019-12-31 22:41:36 +0900

    add sub feature

--- single.php
+++ single.php
@@ -10,6 +10,33 @@
       <span class="post-date"><?php the_time('Y年n月j日'); ?></span>
     </p>
     <?php the_content(); ?>
+    <?php
+      $args = array(
+        'before' => '<div class="pager">',
+        'after' => '</div>',
+        'link_before' => '<span>',
+        'link_after' => '</span>'
+      );
+      wp_link_pages($args); ?>
+    <div class="author">
+      <?php the_author(); ?>
+    </div>
+    <div class="author-description">
+      <?php echo get_the_author_meta('description'); ?>
+    </div>
+    <div class＝"navigation">
+      <? if(get_next_post()): ?>
+        <div class="next-page">次の記事：<?php next_post_link('%link'); ?></div>
+      <?php
+        endif;
+
+        if(get_previous_post()):
+      ?>
+        <div class="prev-page">前の記事：<?php previous_post_link('%link'); ?></div>
+      <?php
+        endif;
+      ?>
+    </div>
   </div>
   <?php endwhile; ?>
 </main>
